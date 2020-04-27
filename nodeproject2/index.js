715a7d5c0da7a08a1bd6ec835c924c45f41cbe51
ghaninf <ghaninf@gmail.com>
2020-04-27 11:21:00 +0700

    Remove node project

--- nodeproject2/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3001;
-
-require('./app/routes/routes.js')(app);
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`);
-});
